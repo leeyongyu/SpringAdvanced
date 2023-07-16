f22ad779f5801a0be65922f3a2ca0937d6554e2e
yonggyu2 <jmk154@naver.com>
2023-07-16 17:31:41 +0900

    2023.07.16 COMMIT

---
+++ src/main/java/spring/advanced/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,40 @@
+package spring.advanced.app.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import spring.advanced.trace.TraceId;
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.hellotrace.HelloTraceV2;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+
+    private final HelloTraceV2 trace;
+
+    public void save(TraceId traceId, String itemId){
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId,"OrderRepository.request()");
+            //저장로직
+            if(itemId.equals("ex")){
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status, e);
+            throw e; // 예외를 다시 밖으로 던져주어야 한다.
+        }
+    }
+
+    private  void sleep(int mills){
+        try{
+            Thread.sleep(mills);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/spring/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,30 @@
+package spring.advanced.app.v2;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.advanced.trace.TraceId;
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.hellotrace.HelloTraceV2;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId){
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId,"OrderService.request()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status, e);
+            throw e; // 예외를 다시 밖으로 던져주어야 한다.
+        }
+    }
+}
